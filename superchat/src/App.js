df4e813be7cd17cf09ee89805ed6f1910127c60c
Nikhil Abraham <nikhil.abraham2001@gmail.com>
2021-08-19 21:19:17 +0530

    updated SignIn and SignOut functions

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -34,4 +34,26 @@ function App() {
   );
 }
 
+const SignIn = () => {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return (
+    <button className="sign-in" onClick={signInWithGoogle}>
+      Sign in with Google
+    </button>
+  );
+};
+
+const SignOut = () => {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
+};
+
 export default App;
